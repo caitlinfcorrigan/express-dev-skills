9aeb95e2972764d12ca5bf7360359f11ebc693bc
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-17 14:26:56 -0700

    add skills data to skills.js

---
+++ models/skills.js
@@ -0,0 +1,24 @@
+// models/skills.js
+
+const skill = [
+  {id: 101, skillName: "HTML", level: "Proficient"},
+  {id: 102, skillName: "CSS", level: "Intermediate"},
+  {id: 105, skillName: "JavaScript", level: "Intermediate"},
+  {id: 201, skillName: "Node.js", level: "Beginner"},
+  {id: 203, skillName: "Express", level: "Beginner"},
+  {id: 301, skillName: "Pyton", level: "Intermediate"}
+]
+  
+    module.exports = {
+      getAll,
+      getOne
+    };
+  
+    function getOne (id) {
+      id = parseInt(id);
+      return skills.find(skill => skill.id === id);
+    }
+      
+    function getAll() {
+      return skills;
+    }
